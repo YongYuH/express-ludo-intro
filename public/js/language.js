3d6ef289e46150c00b6214e1839febbd3a06b3dd
YongYuH <larry830326@hotmail.com>
2016-07-21 00:33:24 +0800

    Add favicon. Modify user number

--- public/js/language.js
+++ public/js/language.js
@@ -21,6 +21,29 @@ var html_text_object = {
         "twitter": '<a href="https://grouptweet.com/LUDO_Now/33a69f55c49ba6310b08007c532ab9cb" target="_blank"><img src="./images/twitter.png" alt="twitter"/></a>',
         "email": '<a href="mailto:ludonow.com@gmail.com" target="_blank"><img src="./images/email.png" alt="email"/></a>',
         "copyright": "© 2016 LUDONOW.COM ALL RIGHTS RESERVED"
+    },    
+    "zh-hk": {
+        "welcome-text": "<br>和全世界的人一起成長",
+        "users-num-before": "<br>簡單填寫<span class=\"english\">Email</span><br>與",
+        "users-num-after": "人<br>一起成長",
+        "join-button": "加入我們",
+        "introduction-text-title": "什麼是<span class=\"english\">Ludo</span>",
+        "introduction-text-content": "<span class=\"english\">Ludo</span>是一個免費的網頁服務，我們希望人們生活中的大小事<br>都可以透過<span class=\"english\">Ludo</span>被重新定義並且直接回饋到每個人身上",
+        "game-text-title": "遊戲",
+        "game-text-content": "簡單的操作方式<br>由一段時間內<br>的互相激勵，<br>讓你輕鬆養成習慣。",
+        "social-text-title": "社交",
+        "social-text-content": "&nbsp;圖示、自動配對、<br>留言三大系統，<br>協助你快速找到<br>志同道合的<span class=\"english\">Ludor。</span>",
+        "expert-text-title": "專家",
+        "expert-text-content": "專家系統讓專業<br>的經驗與知識，<br>直接影響到群眾<br>且得到實際回饋！",
+        "desktop-form-text": "如果想要玩玩看<span class=\"english\">Ludo</span>，<span class=\"english\">Ludo</span>網站將在近期上線<br>在底下的表單留下<span class=\"english\">Email</span>，我們將會通知你最新資訊",
+        "mobile-form-text": "如果想要玩玩看<span class=\"english\">Ludo</span><br><span class=\"english\">Ludo</span>網站將在近期上線<br>在底下的表單留下<span class=\"english\">Email</span><br>我們將會通知你最新資訊",
+        "fill_in": "立即填寫表單",
+        "typeform-div": '<div class="typeform-widget" data-url="https://yongyuh.typeform.com/to/NBIykg" data-text="Ludo" style="width:100%;height:500px;"></div><script>(function(){var qs,js,q,s,d=document,gi=d.getElementById,ce=d.createElement,gt=d.getElementsByTagName,id=\'typef_orm\',b=\'https://s3-eu-west-1.amazonaws.com/share.typeform.com/\';if(!gi.call(d,id)){js=ce.call(d,\'script\');js.id=id;js.src=b+\'widget.js\';q=gt.call(d,\'script\')[0];q.parentNode.insertBefore(js,q)}})()</script><div style="font-family: Sans-Serif;font-size: 12px;color: #999;opacity: 0.5; padding-top: 5px;">Powered by <a href="https://www.typeform.com/examples/forms/?utm_campaign=NBIykg&amp;utm_source=typeform.com-3813118-Basic&amp;utm_medium=typeform&amp;utm_content=typeform-embedded-poweredbytypeform&amp;utm_term=ZH" style="color: #999" target="_blank">Typeform</a></div>',
+        "contact-us-title": "聯絡我們",
+        "facebook": '<a href="https://www.facebook.com/ludonow/?fref=ts" target="_blank"><img src="./images/facebook.png" alt="facebook"/></a>',
+        "twitter": '<a href="https://grouptweet.com/LUDO_Now/33a69f55c49ba6310b08007c532ab9cb" target="_blank"><img src="./images/twitter.png" alt="twitter"/></a>',
+        "email": '<a href="mailto:ludonow.com@gmail.com" target="_blank"><img src="./images/email.png" alt="email"/></a>',
+        "copyright": "© 2016 LUDONOW.COM ALL RIGHTS RESERVED"
     },
     "zh-tw": {
         "welcome-text": "<br>和全世界的人一起成長",
@@ -48,6 +71,7 @@ var html_text_object = {
 }
 
 function renderLanguageText(language) {
+    if (html_text_object[language] == undefined) { language = 'en-us'} 
     $.each(html_text_object[language], function(className, content) {
         $("." + className).html(content);
     });
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,9 @@ var unirest = require('unirest');
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-    unirest.get("https://baskarm28-typeform-v1.p.mashape.com/form/RmhHAY?key=b4c58ff193ed4d038bd43d1925492329368f2c1b&completed=true&limit=10&offset=50")
+    var language = req.acceptsLanguages( 'zh-tw','zh-hk','en','en-us' );
+    if(language == 'zh-tw' | language == 'zh-hk'){
+    unirest.get("https://baskarm28-typeform-v1.p.mashape.com/form/NBIykg?key=b4c58ff193ed4d038bd43d1925492329368f2c1b&completed=true&limit=10&offset=50")
         .header("X-Mashape-Key", "Rx6Hdn8AvZmshP3K2DLNtvzW5I0yp1AYG3djsnsCrAmFd118Yq")
         .header("Accept", "application/json")
         .end(function(result) {
@@ -13,7 +15,17 @@ router.get('/', function(req, res, next) {
             var userNumber = result.body.stats.responses.completed;
             res.render('index', { userNumber: userNumber });
         });
-
+    }
+    else{
+        unirest.get("https://baskarm28-typeform-v1.p.mashape.com/form/RmhHAY?key=b4c58ff193ed4d038bd43d1925492329368f2c1b&completed=true&limit=10&offset=50")
+        .header("X-Mashape-Key", "Rx6Hdn8AvZmshP3K2DLNtvzW5I0yp1AYG3djsnsCrAmFd118Yq")
+        .header("Accept", "application/json")
+        .end(function(result) {
+            var body = result.body;
+            var userNumber = result.body.stats.responses.completed;
+            res.render('index', { userNumber: userNumber });
+        });
+    }
 });
 
 module.exports = router;
