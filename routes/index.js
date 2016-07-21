fd07455ff593f47b0d0b4ef1cf2d93f278b71766
YongYuH <larry830326@hotmail.com>
2016-07-21 16:16:49 +0800

    Modify user number

--- routes/index.js
+++ routes/index.js
@@ -14,17 +14,18 @@ router.get('/', function(req, res, next) {
             // var body = result.body;
             userNumber += Number(result.body.stats.responses.completed);
             // res.render('index', { userNumber: userNumber });
+            unirest.get("https://baskarm28-typeform-v1.p.mashape.com/form/RmhHAY?key=b4c58ff193ed4d038bd43d1925492329368f2c1b&completed=true&limit=10&offset=50")
+                .header("X-Mashape-Key", "Rx6Hdn8AvZmshP3K2DLNtvzW5I0yp1AYG3djsnsCrAmFd118Yq")
+                .header("Accept", "application/json")
+                .end(function(result) {
+                    var body = result.body;
+                    userNumber += Number(result.body.stats.responses.completed);
+                    res.render('index', { userNumber: userNumber });
+                });
         });
     // }
     // else{
-    unirest.get("https://baskarm28-typeform-v1.p.mashape.com/form/RmhHAY?key=b4c58ff193ed4d038bd43d1925492329368f2c1b&completed=true&limit=10&offset=50")
-        .header("X-Mashape-Key", "Rx6Hdn8AvZmshP3K2DLNtvzW5I0yp1AYG3djsnsCrAmFd118Yq")
-        .header("Accept", "application/json")
-        .end(function(result) {
-            var body = result.body;
-            userNumber += Number(result.body.stats.responses.completed);
-            res.render('index', { userNumber: userNumber });
-        });
+    
     // }
 });
 
