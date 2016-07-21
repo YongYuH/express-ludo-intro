ded081a3914b454f943c4766f2be861e0e616c83
YongYuH <larry830326@hotmail.com>
2016-07-21 15:41:28 +0800

    Modify layout and user number

--- routes/index.js
+++ routes/index.js
@@ -4,28 +4,28 @@ var unirest = require('unirest');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
+    var userNumber = 0;
     var language = req.acceptsLanguages( 'zh-tw','zh-hk','en','en-us' );
-    if(language == 'zh-tw' | language == 'zh-hk'){
+    // if(language == 'zh-tw' | language == 'zh-hk'){
     unirest.get("https://baskarm28-typeform-v1.p.mashape.com/form/NBIykg?key=b4c58ff193ed4d038bd43d1925492329368f2c1b&completed=true&limit=10&offset=50")
         .header("X-Mashape-Key", "Rx6Hdn8AvZmshP3K2DLNtvzW5I0yp1AYG3djsnsCrAmFd118Yq")
         .header("Accept", "application/json")
         .end(function(result) {
-            var body = result.body;
-            var userNumber = result.body.stats.responses.completed;
-            res.render('index', { userNumber: userNumber });
+            // var body = result.body;
+            userNumber += Number(result.body.stats.responses.completed);
+            // res.render('index', { userNumber: userNumber });
         });
-    }
-    else{
-        unirest.get("https://baskarm28-typeform-v1.p.mashape.com/form/RmhHAY?key=b4c58ff193ed4d038bd43d1925492329368f2c1b&completed=true&limit=10&offset=50")
+    // }
+    // else{
+    unirest.get("https://baskarm28-typeform-v1.p.mashape.com/form/RmhHAY?key=b4c58ff193ed4d038bd43d1925492329368f2c1b&completed=true&limit=10&offset=50")
         .header("X-Mashape-Key", "Rx6Hdn8AvZmshP3K2DLNtvzW5I0yp1AYG3djsnsCrAmFd118Yq")
         .header("Accept", "application/json")
         .end(function(result) {
             var body = result.body;
-            var userNumber = result.body.stats.responses.completed;
+            userNumber += Number(result.body.stats.responses.completed);
             res.render('index', { userNumber: userNumber });
         });
-    }
+    // }
 });
 
 module.exports = router;
