0255dfbdd43450e1318d988113194472a57055fa
SeanGo <sean820117@gmail.com>
2016-07-24 01:25:34 +0800

    fix shut down bug

--- routes/index.js
+++ routes/index.js
@@ -10,28 +10,32 @@ router.get('/', function(req, res, next) {
 
 function get_join_number() {
     // var language = req.acceptsLanguages( 'zh-tw','zh-hk','en','en-us' );
-    try {
-        console.log("get join number");
-        userNumber = 0;
-        unirest.get("https://baskarm28-typeform-v1.p.mashape.com/form/NBIykg?key=b4c58ff193ed4d038bd43d1925492329368f2c1b&completed=true&limit=10&offset=50")
-            .header("X-Mashape-Key", "Rx6Hdn8AvZmshP3K2DLNtvzW5I0yp1AYG3djsnsCrAmFd118Yq")
-            .header("Accept", "application/json")
-            .end(function(result) {
+    
+    console.log("get join number");
+    userNumber = 0;
+    unirest.get("https://baskarm28-typeform-v1.p.mashape.com/form/NBIykg?key=b4c58ff193ed4d038bd43d1925492329368f2c1b&completed=true&limit=10&offset=50")
+        .header("X-Mashape-Key", "Rx6Hdn8AvZmshP3K2DLNtvzW5I0yp1AYG3djsnsCrAmFd118Yq")
+        .header("Accept", "application/json")
+        .end(function(result) {
+            try {
                 userNumber += Number(result.body.stats.responses.completed);
-        });
-        
-        
-        unirest.get("https://baskarm28-typeform-v1.p.mashape.com/form/RmhHAY?key=b4c58ff193ed4d038bd43d1925492329368f2c1b&completed=true&limit=10&offset=50")
-            .header("X-Mashape-Key", "Rx6Hdn8AvZmshP3K2DLNtvzW5I0yp1AYG3djsnsCrAmFd118Yq")
-            .header("Accept", "application/json")
-            .end(function(result) {
-                var body = result.body;
+            } catch(err) {
+                console.log("userNumber error : "  + err)
+            }
+    });
+    
+    
+    unirest.get("https://baskarm28-typeform-v1.p.mashape.com/form/RmhHAY?key=b4c58ff193ed4d038bd43d1925492329368f2c1b&completed=true&limit=10&offset=50")
+        .header("X-Mashape-Key", "Rx6Hdn8AvZmshP3K2DLNtvzW5I0yp1AYG3djsnsCrAmFd118Yq")
+        .header("Accept", "application/json")
+        .end(function(result) {
+            try {
                 userNumber += Number(result.body.stats.responses.completed);
-                
-        });
-    } catch(err) {
-        console.log("userNumber error : "  + err)
-    }
+            } catch(err) {
+                console.log("userNumber error : "  + err)
+            }
+    });
+    
 }
 
 get_join_number();
